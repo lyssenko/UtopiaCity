365215927c64333262e34eff026195eef5a9c3b7
Lyssenko Roman <lyssenkori@gmail.com>
2021-07-05 20:24:15 +0600

    Add models Restaurant and Status

---
+++ UtopiaCity/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UtopiaCity.Enums;
+
+namespace UtopiaCity.Models
+{
+    public class Restaurant
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public Status Status { get; set; }
+        public int Seats { get; set; }
+    }
+}
